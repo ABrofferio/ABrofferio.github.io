079f20c348f5f3790cd171abdd8857d2b1ceedf4
Aja Brofferio <aja.brofferio@gmail.com>
2016-07-30 15:00:56 -0700

    push js, css, and html changes

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/*Main AngularJS Web Application*/
+
+var app = angular.module('app', ['ngRoute']);
+
+/*Configure the Routes*/
+app.config(function($routeProvider) {
+	$routeProvider
+		.when("/", {
+			templateUrl: "partials/home.html",
+			controller: "homeController"
+		})
+		.when('/about', {
+			templateUrl: 'partials/about.html',
+			controller: 'aboutController'
+		})
+		.when('/skills', {
+			templateUrl: 'partials/skills.html',
+			controller: 'skillsController'
+		})
+		.when('/portfolio', {
+			templateUrl: 'partials/portfolio.html',
+			controller: "portfolioController"
+		})
+		.when('/contact', {
+			templateUrl: 'partials/contact.html',
+			controller: 'contactController'
+		});
+});
+app.controller('homeController', function($scope) {
+	$scope.name = "home";
+});
+app.controller('aboutController', function($scope) {
+	$scope.name = "about";
+});
+app.controller('skillsController', function($scope) {
+	$scope.name = "skills";
+});
+app.controller('portfolioController', function($scope) {
+	$scope.name = "portfolio";
+});
+app.controller('contactController', function($scope) {
+	$scope.name = "contact";
+});
